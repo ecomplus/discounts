7a17c6323ac61c54f649ba708820c5846403065a
Leonardo Matos <leomp120894@gmail.com>
2021-09-12 17:44:47 -0300

    feat(freebies-rules): handle new `check_product_ids` config option for freebies [#18]
    fixes https://github.com/ecomplus/discounts/issues/18

--- routes/ecom/modules/apply-discount.js
+++ routes/ecom/modules/apply-discount.js
@@ -2,143 +2,14 @@
 
 const ecomUtils = require('@ecomplus/utils')
 
-const validateDateRange = rule => {
-  // filter campaings by date
-  const timestamp = Date.now()
-  if (rule.date_range) {
-    if (rule.date_range.start && new Date(rule.date_range.start).getTime() > timestamp) {
-      return false
-    }
-    if (rule.date_range.end && new Date(rule.date_range.end).getTime() < timestamp) {
-      return false
-    }
-  }
-  return true
-}
-
-const validateCustomerId = (rule, params) => {
-  if (
-    Array.isArray(rule.customer_ids) &&
-    rule.customer_ids.length &&
-    rule.customer_ids.indexOf(params.customer && params.customer._id) === -1
-  ) {
-    // unavailable for current customer
-    return false
-  }
-  return true
-}
-
-const checkOpenPromotion = rule => {
-  return !rule.discount_coupon && !rule.utm_campaign &&
-    (!Array.isArray(rule.customer_ids) || !rule.customer_ids.length)
-}
-
-const getValidDiscountRules = (discountRules, params, items) => {
-  if (Array.isArray(discountRules) && discountRules.length) {
-    // validate rules objects
-    return discountRules.filter(rule => {
-      if (!rule || !validateCustomerId(rule, params)) {
-        return false
-      }
-
-      if (Array.isArray(rule.product_ids) && Array.isArray(items)) {
-        const checkProductId = item => {
-          return (!rule.product_ids.length || rule.product_ids.indexOf(item.product_id) > -1)
-        }
-        // set/add discount value from lowest item price
-        let value
-        if (rule.discount_lowest_price) {
-          items.forEach(item => {
-            const price = ecomUtils.price(item)
-            if (price > 0 && checkProductId(item) && (!value || value > price)) {
-              value = price
-            }
-          })
-        } else if (rule.discount_kit_subtotal) {
-          value = 0
-          items.forEach(item => {
-            const price = ecomUtils.price(item)
-            if (price > 0 && checkProductId(item)) {
-              value += price * item.quantity
-            }
-          })
-        }
-        if (value) {
-          if (rule.discount && rule.discount.value) {
-            if (rule.discount.type === 'percentage') {
-              value *= rule.discount.value / 100
-            } else {
-              value += rule.discount.value
-            }
-          }
-          rule.discount = {
-            ...rule.discount,
-            type: 'fixed',
-            value
-          }
-        }
-      }
-      if (!rule.discount || !rule.discount.value) {
-        return false
-      }
-
-      return validateDateRange(rule)
-    })
-  }
-
-  // returns array anyway
-  return []
-}
-
-const matchDiscountRule = (discountRules, params) => {
-  // try to match a promotion
-  if (params.discount_coupon) {
-    // match only by discount coupon
-    return {
-      discountRule: discountRules.find(rule => {
-        return rule.case_insensitive
-          ? typeof rule.discount_coupon === 'string' &&
-            rule.discount_coupon.toUpperCase() === params.discount_coupon.toUpperCase()
-          : rule.discount_coupon === params.discount_coupon
-      }),
-      discountMatchEnum: 'COUPON'
-    }
-  }
-
-  // try to match by UTM campaign first
-  if (params.utm && params.utm.campaign) {
-    const discountRule = discountRules.find(rule => {
-      return rule.case_insensitive
-        ? typeof rule.utm_campaign === 'string' &&
-          rule.utm_campaign.toUpperCase() === params.utm.campaign.toUpperCase()
-        : rule.utm_campaign === params.utm.campaign
-    })
-    if (discountRule) {
-      return {
-        discountRule,
-        discountMatchEnum: 'UTM'
-      }
-    }
-  }
-
-  // then try to match by customer
-  if (params.customer && params.customer._id) {
-    const discountRule = discountRules.find(rule => Array.isArray(rule.customer_ids) &&
-      rule.customer_ids.indexOf(params.customer._id) > -1)
-    if (discountRule) {
-      return {
-        discountRule,
-        discountMatchEnum: 'CUSTOMER'
-      }
-    }
-  }
-
-  // last try to match by open promotions
-  return {
-    discountRule: discountRules.find(checkOpenPromotion),
-    discountMatchEnum: 'OPEN'
-  }
-}
+const {
+  validateDateRange,
+  validateCustomerId,
+  checkOpenPromotion,
+  getValidDiscountRules,
+  matchDiscountRule,
+  checkCampaignProducts
+} = require('./../../../lib/helpers')
 
 module.exports = appSdk => {
   return (req, res) => {
@@ -274,6 +145,7 @@ module.exports = appSdk => {
         const validFreebiesRules = config.freebies_rules.filter(rule => {
           return validateDateRange(rule) &&
             validateCustomerId(rule, params) &&
+            checkCampaignProducts(rule.check_product_ids, params) &&
             Array.isArray(rule.product_ids) &&
             rule.product_ids.length
         })
@@ -327,26 +199,13 @@ module.exports = appSdk => {
     if (discountRules.length) {
       const { discountRule, discountMatchEnum } = matchDiscountRule(discountRules, params)
       if (discountRule) {
-        const campaignProducts = discountRule.product_ids
-        if (Array.isArray(campaignProducts) && campaignProducts.length) {
-          // must check at least one campaign product on cart
-          let hasProductMatch
-          if (params.items && params.items.length) {
-            for (let i = 0; i < campaignProducts.length; i++) {
-              if (params.items.find(item => item.quantity && item.product_id === campaignProducts[i])) {
-                hasProductMatch = true
-                break
-              }
-            }
-          }
-          if (!hasProductMatch) {
-            return res.send({
-              available_extra_discount: response.available_extra_discount,
-              invalid_coupon_message: params.lang === 'pt_br'
-                ? 'Nenhum produto da promoção está incluído no carrinho'
-                : 'No promotion products are included in the cart'
-            })
-          }
+        if (!checkCampaignProducts(discountRule.product_ids, params)) {
+          return res.send({
+            available_extra_discount: response.available_extra_discount,
+            invalid_coupon_message: params.lang === 'pt_br'
+              ? 'Nenhum produto da promoção está incluído no carrinho'
+              : 'No promotion products are included in the cart'
+          })
         }
 
         const excludedProducts = discountRule.excluded_product_ids
